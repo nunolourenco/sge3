6cc06d6f31d699b9dde1da7eeabb7d777f51ab2f
Nuno Lourenço <namlourenco@gmail.com>
2019-09-20 17:37:34 +0100

    Added dumps.

--- sge/sge/logger.py
+++ sge/sge/logger.py
@@ -1,41 +1,39 @@
 import numpy as np
 from sge.parameters import params
-from sge.utilities.run_info_orm import Base, EvolutionaryRun, PopulationSample, Parameters
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+import json
+import os
 
-
-engine = create_engine('sqlite:///%s.db' % params['EXPERIMENT_NAME'])
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 
 def evolution_progress(generation, pop):
     fitness_samples = [i['fitness'] for i in pop]
-    new_generation = EvolutionaryRun(run=params['RUN'], generation=generation, best_fitness=np.min(fitness_samples),
-                                     mean_fitness=np.mean(fitness_samples),std_fitness=np.std(fitness_samples))
-    session.add(new_generation)
-    session.cpythonommit()
+    data = '%4d\t%.6e\t%.6e\t%.6e' % (generation, np.min(fitness_samples), np.mean(fitness_samples), np.std(fitness_samples))
     if params['VERBOSE']:
-        print(new_generation)
+        print(data)
+    save_progress_to_file(data)
     if generation % params['SAVE_STEP'] == 0:
         save_step(generation, pop)
 
 
+def save_progress_to_file(data):
+    with open('%s/run_%d/progress_report.csv' % (params['EXPERIMENT_NAME'], params['RUN']), 'a') as f:
+        f.write(data + '\n')
+
+
 def save_step(generation, population):
-    pop_dump = PopulationSample(run=params['RUN'], generation=generation,population=population)
-    session.add(pop_dump)
-    session.commit()
+    c = json.dumps(population)
+    open('%s/run_%d/iteration_%d.json' % (params['EXPERIMENT_NAME'], params['RUN'], generation), 'a').write(c)
 
 
 def save_parameters():
     params_lower = dict((k.lower(), v) for k, v in params.items())
-    new_parameters = Parameters(**params_lower)
-    session.add(new_parameters)
-    session.commit()
+    c = json.dumps(params_lower)
+    open('%s/run_%d/parameters.json' % (params['EXPERIMENT_NAME'], params['RUN']), 'a').write(c)
 
 
 def prepare_dumps():
-    Base.metadata.create_all(engine)
-    Base.metadata.bind = engine
+    try:
+        os.makedirs('%s/run_%d' % (params['EXPERIMENT_NAME'], params['RUN']))
+    except FileExistsError as e:
+        pass
     save_parameters()
