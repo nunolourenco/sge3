8bf53eae1c28d4c5f184f8e6e07c451bece52850
Jessica Mégane <jessicac@dei.uc.pt>
2026-03-26 17:09:23 +0000

    Mutation now doesn't allow that trees surpass te depth limit

--- sge/sge/operators/mutation.py
+++ sge/sge/operators/mutation.py
@@ -16,7 +16,7 @@ def mutate(p, pmutation):
             if random.random() < pmutation:
                 current_value = p['genotype'][at_gene][position_to_mutate]
                 choices = []
-                if p['tree_depth'] >= grammar.get_max_depth():
+                if p['tree_depth'] >= (grammar.get_max_depth() - grammar.get_shortest_path()[(nt,'NT')][0]):
                     possibilities = grammar.get_shortest_path()[(nt,'NT')][1:]
                     rule = random.choice(possibilities)
                     expansion_possibility = grammar.get_grammar()[nt].index(rule)
