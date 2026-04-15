b624f9d2e1683680c98d678429481257d4d1269f
Jessica Mégane <jessicac@dei.uc.pt>
2026-04-15 19:20:27 +0100

    mutation now varies according to codon depth

--- sge/sge/operators/mutation.py
+++ sge/sge/operators/mutation.py
@@ -1,5 +1,5 @@
 import copy
-import random
+import numpy as np
 import sge.grammar as grammar
 
 
@@ -13,16 +13,20 @@ def mutate(p, pmutation):
         temp = p['mapping_values']
         mapped = temp[at_gene]
         for position_to_mutate in range(0, mapped):
-            if random.random() < pmutation:
+            if np.random.uniform() < pmutation:
                 current_value = p['genotype'][at_gene][position_to_mutate]
-                choices = []
-                if p['tree_depth'] >= grammar.get_max_depth():
-                    possibilities = grammar.get_shortest_path()[(nt,'NT')][1:]
-                    rule = random.choice(possibilities)
+                current_depth = current_value[1]
+                if current_depth >= (grammar.get_max_depth() - grammar.get_shortest_path()[(nt,'NT')][0]):
+                    choices = grammar.get_shortest_path()[(nt,'NT')][1:]
+                    current_sym = grammar.get_grammar()[nt][current_value[0]]
+                    if current_sym in choices and len(choices) > 1:
+                        choices.remove(current_sym)
+                    rule = choices[np.random.randint(0, len(choices))]
                     expansion_possibility = grammar.get_grammar()[nt].index(rule)
-                    p['genotype'][at_gene][position_to_mutate] = expansion_possibility
+                    p['genotype'][at_gene][position_to_mutate] = [expansion_possibility, current_depth]
+
                 else:
                     choices = list(range(0, size_of_genes[nt]))
-                    choices.remove(current_value)
-                    p['genotype'][at_gene][position_to_mutate] = random.choice(choices)
+                    choices.remove(current_value[0])
+                    p['genotype'][at_gene][position_to_mutate] = [np.random.choice(choices), current_depth]
     return p
